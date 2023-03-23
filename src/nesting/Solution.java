ad5747d5958139047920265a7cd26d4f255a463b
Nilesh Wani <n.wani@maxxton.com>
2023-03-23 14:13:21 +0100

    Core Java Features

---
+++ src/nesting/Solution.java
@@ -0,0 +1,43 @@
+package nesting;
+
+import java.util.Stack;
+
+/**
+ * Solution for Codility lesson: https://app.codility.com/programmers/lessons/7-stacks_and_queues/nesting/
+ * 
+ * Idea: Simple! Use Stack to check the matching brackets.
+ * 
+ * Traverse the characters in the String. When you see the opening bracket, push that in the Stack. When you see the closing bracket, check if stack has matching opening bracket. At the end, stack
+ * should be empty.
+ */
+public class Solution {
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    solution.solution(")(");
+  }
+
+  public int solution(String S) {
+    Stack<Character> stack = new Stack<>();
+
+    for (char c : S.toCharArray()) {
+      if (c == '(') {
+        stack.push(c);
+      }
+      else {
+        if (stack.empty()) {
+          return 0;
+        }
+
+        Character previousChar = stack.pop();
+
+        if ((c == ')' && previousChar != '(')) {
+          return 0;
+        }
+      }
+    }
+
+    return stack.empty() ? 1 : 0;
+  }
+
+}
